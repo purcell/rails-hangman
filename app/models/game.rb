b5fb25558639cd3e61d43aeaead913b38461cacc
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-15 11:15:04 +1200

    Show a masked version of the word's letters

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,18 @@
 class Game < ActiveRecord::Base
   before_create :choose_a_word
 
+  def masked_word_letters
+    word.chars.map do |c|
+      case c
+      when /[a-z]/i then nil
+      else c
+      end
+    end
+  end
+
   private
 
   def choose_a_word
-    self.word = File.read(Rails.root + "config/words.txt").split(/\n/).sample
+    self.word ||= File.read(Rails.root + "config/words.txt").split(/\n/).sample
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,14 +5,26 @@ RSpec.describe Game, type: :model do
   let(:dictionary) { File.read(Rails.root + "config/words.txt").split(/\n/) }
 
   context "when creating a game" do
+    it "can be created with a word" do
+      g = Game.create!(word: "hello")
+      expect(g.word).to eql("hello")
+    end
+
     it "chooses a random word" do
       srand(5150)
-      g1 = Game.create
+      g1 = Game.create!
       expect(dictionary).to include(g1.word)
+
       srand(812)
-      g2 = Game.create
+      g2 = Game.create!
       expect(dictionary).to include(g2.word)
+
       expect(g2.word).not_to eql(g1.word)
+    end
+
+    it "provides a masked version of the word's letters" do
+      expect(Game.create!(word: "mad props").masked_word_letters).to eql([nil, nil, nil, " ", nil, nil, nil, nil, nil])
+      expect(Game.create!(word: "it's good").masked_word_letters).to eql([nil, nil, "'", nil, " ", nil, nil, nil, nil])
     end
   end
 end

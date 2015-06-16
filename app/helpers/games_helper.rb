ac7a99c0d1e36d95e0e4484f652f716ddf8aae73
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-16 13:42:53 +1200

    Extract keyboard button into helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,4 +2,9 @@ module GamesHelper
   def new_game_button
     button_to "New game", games_url, method: :post, class: 'btn btn-primary'
   end
+
+  def create_guess_button(game, letter, html_options={})
+    default_opts = {method: :post, disabled: game.already_guessed?(letter), data: {'disable-with' => letter}}
+    button_to(letter, game_guesses_url(game, letter: letter), html_options.merge(default_opts))
+  end
 end
